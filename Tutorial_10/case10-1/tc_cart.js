81678411c3bbe60d713ab2735f736a3bf45c7a04
Miles-Wiser <129561396+Miles-Wiser@users.noreply.github.com>
2025-05-09 10:48:51 -0600

    Add files via upload

--- Tutorial_10/case10-1/tc_cart.js
+++ Tutorial_10/case10-1/tc_cart.js
@@ -5,10 +5,41 @@
    Tutorial 10
    Case Problem 1
 
-   Author: 
-   Date:   
+   Author: Miles Wiser
+   Date:   5/9/2025
    
    Filename: tc_cart.js
 	
 */
 
+
+var orderTotal = 0;
+
+// Holds the table and all information
+var cartHTML = "<table><tr><th>Item</th>" + "<th>Description</th>" +
+   "<th>Price</th>" + "<th>Qty</th>" + "<th>Total</th></tr>";
+
+// Adds a row to cartHTML
+for (var i = 0; i < 4; i++) {
+   // item img
+   cartHTML += "<tr><td><img src='tc_" + item[i] +
+      ".png' alt='" + item[i] + "' /></td>";
+
+   // item description, price, qty
+   cartHTML += "<td>"+ itemDescription[i] + "</td>" +
+      "<td>$"+ itemPrice[i] + "</td>" +
+      "<td>"+ itemQty[i] + "</td>";
+
+   var itemCost = itemPrice[i] * itemQty[i];
+   cartHTML += "<td>$" + itemCost + "</td></tr>";
+
+   // add totals for later use
+   orderTotal += itemCost;
+}
+
+// display subtotal in a new row
+cartHTML += "<tr> <td colspan='4'>Subtotal</td>" +
+   "<td>$" + orderTotal + "</td> </tr> </table>"
+
+// assign the cartHTML table to the div element
+document.getElementById("cart").innerHTML = cartHTML;
